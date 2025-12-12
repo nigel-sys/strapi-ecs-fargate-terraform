822f14799ea914df5e6a681d708c8fa09360e550
nigel-sys <pratyushnbaxla79@gmail.com>
2025-12-12 10:42:45 +0530

    fix login to ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,36 +46,6 @@ data "aws_ami" "latest_al2023" {
   }
 }
 
-resource "aws_iam_role" "ec2_role" {
-  name = "pratyush_strapi_ec2_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_read_only" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_ssm" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "pratyush_strapi_ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 resource "aws_instance" "strapi_server" {
   ami                    = data.aws_ami.latest_al2023.id
   key_name               = "pratyush_baxla_key"
