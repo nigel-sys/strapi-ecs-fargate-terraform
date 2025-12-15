15f15a2f55057432b6dc06b87097ab1645dff783
nigel-sys <pratyushnbaxla79@gmail.com>
2025-12-15 16:13:31 +0530

    remove db waiting command

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -36,14 +36,6 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION=ap-south-1
 
-echo "Waiting for PostgreSQL to be ready..."
-# Wait until PostgreSQL is ready
-until docker run --rm -e PGPASSWORD=${DATABASE_PASSWORD} postgres:latest psql -h ${DATABASE_HOST} -U ${DATABASE_USERNAME} -d ${DATABASE_NAME} -c '\q'; do
-  echo "PostgreSQL is unavailable - sleeping"
-  sleep 5
-done
-echo "PostgreSQL is up - continuing"
-
 # Login to ECR
 ECR_PASSWORD=$(aws ecr get-login-password --region ap-south-1)
 docker login -u AWS -p "$ECR_PASSWORD" 301782007642.dkr.ecr.ap-south-1.amazonaws.com
