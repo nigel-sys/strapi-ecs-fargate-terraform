e23e1765f2cad426030782bc87376599c6e74f38
nigel-sys <pratyushnbaxla79@gmail.com>
2025-12-16 00:59:49 +0530

    Terraform Deploy

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "pratyush-baxla-strapi-terraform"
+    bucket  = "pratyush-baxla-ecs-strapi-terraform"
     key     = "strapi/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
