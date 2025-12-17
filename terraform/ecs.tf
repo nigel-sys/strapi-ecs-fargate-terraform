5aca6b99c4e28d86125d9c0a8e70b4a19db6fdb9
nigel-sys <pratyushnbaxla79@gmail.com>
2025-12-17 16:15:20 +0530

    enable container insights

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,5 +1,10 @@
 resource "aws_ecs_cluster" "strapi" {
   name = "pratyush-baxla-strapi-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
 
 resource "aws_cloudwatch_log_group" "strapi" {
