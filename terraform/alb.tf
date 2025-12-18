26a319f4e3cacb2b7ea5622c6e526658e8fa368b
nigel-sys <pratyushnbaxla79@gmail.com>
2025-12-18 16:12:33 +0530

    Add ALB and private subnet

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,52 +1,52 @@
-# resource "aws_lb" "strapi_alb" {
-#   name               = "pratyush-strapi-alb"
-#   load_balancer_type = "application"
-#   internal           = false
-#   subnets            = [
-#     "subnet-0dcf98e23a5861550",  
-#     "subnet-0342cfb028d6aff5a"   
-#   ]
-#   security_groups    = [aws_security_group.alb_sg.id]
-# }
+resource "aws_lb" "strapi_alb" {
+  name               = "pratyush-strapi-alb"
+  load_balancer_type = "application"
+  internal           = false
+  subnets            = [
+    "subnet-0dcf98e23a5861550",  
+    "subnet-0342cfb028d6aff5a"   
+  ]
+  security_groups    = [aws_security_group.alb_sg.id]
+}
 
-# resource "aws_security_group" "alb_sg" {
-#   name   = "pratyush-strapi-alb-sg"
-#   vpc_id = data.aws_vpc.default.id
+resource "aws_security_group" "alb_sg" {
+  name   = "pratyush-strapi-alb-sg"
+  vpc_id = data.aws_vpc.default.id
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-# resource "aws_lb_target_group" "strapi_tg" {
-#   name        = "pratyush-strapi-tg"
-#   port        = 1337
-#   protocol    = "HTTP"
-#   vpc_id      = data.aws_vpc.default.id
-#   target_type = "ip"
+resource "aws_lb_target_group" "strapi_tg" {
+  name        = "pratyush-strapi-tg"
+  port        = 1337
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "ip"
 
-#   health_check {
-#     path = "/"
-#   }
-# }
+  health_check {
+    path = "/"
+  }
+}
 
-# resource "aws_lb_listener" "strapi_listener" {
-#   load_balancer_arn = aws_lb.strapi_alb.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "strapi_listener" {
+  load_balancer_arn = aws_lb.strapi_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.strapi_tg.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.strapi_tg.arn
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -38,11 +38,51 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+data "aws_subnet" "public_1b" {
+  id = "subnet-0dcf98e23a5861550"
+}
+
+resource "aws_subnet" "pratyush_private_1b" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.220.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name  = "pratyush-private-1b"
+    Owner = "pratyush"
   }
 }
 
-#
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "pratyush_nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = data.aws_subnet.public_1b.id
+
+  tags = {
+    Name  = "pratyush-nat"
+    Owner = "pratyush"
+  }
+}
+
+resource "aws_route_table" "pratyush_private_rt" {
+  vpc_id = data.aws_vpc.default.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.pratyush_nat.id
+  }
+
+  tags = {
+    Name  = "pratyush-private-rt"
+    Owner = "pratyush"
+  }
+}
+
+resource "aws_route_table_association" "pratyush_private_assoc" {
+  subnet_id      = aws_subnet.pratyush_private_1b.id
+  route_table_id = aws_route_table.pratyush_private_rt.id
+}
